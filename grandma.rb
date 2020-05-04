5f74f6217bd935ed044460df0f88d4ea7cd5b8a2
Jonathan East <jonathan_east@ymail.com>
2020-05-04 20:41:25 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase.upcase
+  elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
   elsif phrase.downcase!
     return "HUH?! SPEAK UP, SONNY!"
